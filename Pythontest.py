6e2a6b2d04a83396f21ed382fac899f9d17fee8c
misterharu <misterharu9@gmail.com>
2023-12-18 20:52:00 +0900

    231218 update

--- Pythontest.py
+++ Pythontest.py
@@ -1,10 +1,15 @@
-numbers = []
-numbers.append(5)
-numbers.append(8)
-numbers.extend([6, 7, 8])
-del numbers[1]
-numbers.insert(3, 30)
+# 화씨 온도에서 섭씨 온도로 바꿔 주는 함수
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
 
-print(numbers)
-print(len(numbers))
 
+temperature_list = [40, 15, 32, 64, -4, 11]
+print("화씨 온도 리스트: {}".format(temperature_list))  # 화씨 온도 출력
+
+# 리스트의 값들을 화씨에서 섭씨로 변환하는 코드
+i = 0
+while i < len(temperature_list):
+    temperature_list[i] = round(fahrenheit_to_celsius(temperature_list[i]), 1)
+    i += 1
+    
+print("섭씨 온도 리스트: {}".format(temperature_list))  # 섭씨 온도 출력
